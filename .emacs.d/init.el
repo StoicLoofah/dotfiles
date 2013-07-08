6f35aa55a0d0182d4a89129d226d0f42002a01d0
Kevin Leung <kkleung89@gmail.com>
2013-07-08 08:25:06 -0700

    replacing .emacs with .emacs.d/init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,7 +18,7 @@
 (require 'compile)
 (ido-mode t)
 (menu-bar-mode -1)
-(normal-erase-is-backspace-mode 1)
+;(normal-erase-is-backspace-mode 1)
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (setq column-number-mode t)
@@ -72,6 +72,44 @@
 (load "js-config.el")
 (add-to-list 'load-path "~/.emacs.d/jade-mode") ;; github.com/brianc/jade-mode
 (require 'sws-mode)
-(require 'jade-mode)    
+(require 'jade-mode)
 (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+
+;; PERSONAL
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(add-to-list 'load-path "~/.emacs.d/")
+(require 'auto-complete)
+(global-auto-complete-mode t)
+
+;experiment with django template formatting; not fun
+;(load "~/.emacs.d/nxhtml/autostart.el")
+;(setq mumamo-background-colors nil)
+;(add-to-list 'auto-mode-alist '("\\.html$" . django-html-mumamo-mode))
+
+(setq inhibit-startup-message t
+inhibit-startup-echo-area-message t)
+
+(set-face-attribute 'default nil :height 100)
+
+(add-to-list 'default-frame-alist '(left . 0))
+(add-to-list 'default-frame-alist '(top . 0))
+(add-to-list 'default-frame-alist '(height . 52))
+(add-to-list 'default-frame-alist '(width . 200))
+;(split-window-horizontally)
+
+;(require 'multi-web-mode)
+;(setq mweb-default-major-mode 'html-mode)
+;(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+;                  (javascript-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+;                  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+;(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+;(multi-web-global-mode 1)
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80))
+(setq tab-width 4)
+(setq indent-tabs-mode nil)
+(setq indent-line-function 'insert-tab)
